02816e909a5fae49f89550fdcbb359faf8eee804
Ronald <ronystivens@hotmail.com>
2020-10-16 12:23:27 +0200

    added dependencies and structure for the page

---
+++ lyrics-finder/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../logo.svg';
+
+const Header = () => {
+    return (
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>Mis canciones</h1>
+            <img src={logo} className="App-logo" alt="logo" />
+        </header>
+    )
+}
+
+export default Header;
