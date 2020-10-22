81f3158152e3804c2611768603536b4290736db3
Ronald <ronystivens@hotmail.com>
2020-10-22 19:40:34 +0200

    added search buttons

--- lyrics-finder/src/components/Finder.js
+++ lyrics-finder/src/components/Finder.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import IconButton from '@material-ui/core/IconButton';
-import SearchIcon from '@material-ui/icons/Home';
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
 
 
 const useStyles = makeStyles(theme => ({
@@ -21,10 +22,15 @@ const useStyles = makeStyles(theme => ({
 const Finder = () => {
     const classes = useStyles();
     return (
-        <form className={classes.root} noValidate autoComplete="off">
-            <TextField id="standard-basic" label="Standard" />
-            <TextField id="filled-basic" label="Filled" variant="filled" />
-            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        <form className={classes.root} autoComplete="on">
+            <IconButton color="primary" type="reset">
+                <HomeIcon></HomeIcon>
+            </IconButton>
+            <TextField id="artist" name="artist" label="Artista" variant="outlined" size="small" required/>
+            <TextField id="song" name="song" label="Canción" variant="outlined" size="small" required/>
+            <IconButton color="primary" type="submit">
+                <SearchIcon></SearchIcon>
+            </IconButton>
         </form>
 
     )
