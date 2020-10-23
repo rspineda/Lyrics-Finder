61fd821de2d89437e006f5c776909217bc55c8fe
Ronald <ronystivens@hotmail.com>
2020-10-23 23:53:08 +0200

    handled fetchto artist and lyrics api

--- lyrics-finder/src/App.js
+++ lyrics-finder/src/App.js
@@ -12,7 +12,7 @@ import Finder from './components/Finder';
 function App() {
 
   //cargar canciones desde el localStorage:
-  let mySongsInit = JSON.parse(localStorage.getItem('mySongs'));
+  let mySongsInit = JSON.parse(localStorage.getItem('mySongs')) || [];
 
   let searchInit = {
     artist: '',
@@ -27,6 +27,37 @@ function App() {
   const [currentSong, setCurrentSong] = useState({});
   //errores de busqueda
   const [error, setError] = useState(false);
+
+  useEffect(()=>{
+    localStorage.getItem('mySongs');
+    const getData = async () => {
+      const {artist, song} = search;
+      try{
+        let artistUrl = `https://theaudiodb.com/api/v1/json/1/search.php?s=${artist}`;
+        let songUrl = `https://api.lyrics.ovh/v1/${artist}/${song}`;
+        let artistRes = await fetch(artistUrl);
+        let songRes = await fetch(songUrl);
+        let artistJson = await artistRes.json();
+        let songJson = await songRes.json(); 
+
+        console.log('el Json del artista',artistJson);
+        console.log('el Json de la cancion',songJson);
+
+      }catch(error){
+        setSearch({
+          artist: artist,
+          song: song,
+          request: false
+        });
+        setError(true);
+      }
+    }
+    if(!search.request){ //si la petición no se hace devuelvo un efecto vacío para que no se ejecute mas veces
+      return;
+    }else{
+      getData();
+    }
+  }, [search]);
 
   return (
     <Router>
