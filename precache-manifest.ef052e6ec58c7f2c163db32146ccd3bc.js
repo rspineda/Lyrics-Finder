5e638bc2b0d243045acc2c73896acc26ea515038
Ronald <ronystivens@hotmail.com>
2020-11-01 12:51:51 +0100

    Updates

---
+++ precache-manifest.ef052e6ec58c7f2c163db32146ccd3bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "256ff8f1b1075b545394a7c75df88036",
+    "url": "/Lyrics-Finder/index.html"
+  },
+  {
+    "revision": "23d76796fb0da843dc70",
+    "url": "/Lyrics-Finder/static/css/main.1e4663ff.chunk.css"
+  },
+  {
+    "revision": "16a13f6de23c4af7a505",
+    "url": "/Lyrics-Finder/static/js/2.ddc245cd.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/Lyrics-Finder/static/js/2.ddc245cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23d76796fb0da843dc70",
+    "url": "/Lyrics-Finder/static/js/main.cbb8eb15.chunk.js"
+  },
+  {
+    "revision": "2ee7060e7ca6bd4b9f32",
+    "url": "/Lyrics-Finder/static/js/runtime-main.99816dc6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Lyrics-Finder/static/media/logo.5d5d9eef.svg"
+  }
+]);
